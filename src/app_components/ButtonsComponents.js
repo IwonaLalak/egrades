cc439b03328c1204bdeb45bb97421339e04bf48b
iwona Lalak <lalak.iwona@gmail.com>
2019-02-12 16:40:56 +0100

    buttons components

--- src/app_components/ButtonsComponents.js
+++ src/app_components/ButtonsComponents.js
@@ -37,3 +37,238 @@ CustomButton.defaultProps = {
     color:'secondary',    // primary, secondary, success, danger, warning, info, light, dark
     size:'md', // sm, md, lg
 };
+
+export const ButtonAdd = (props) => {
+    return (
+        <Button
+            onClick={props.onClick}
+            size={props.size}
+            color={'success'}
+            title={props.title}
+            style={props.style}
+            {...props}
+        >
+            <i className={'fa fa-plus'} style={{marginRight:'5px'}}></i> {props.label}
+        </Button>
+    );
+};
+
+ButtonAdd.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    label: PropTypes.string,
+    size:PropTypes.string,
+    style: PropTypes.object,
+    title: PropTypes.string,
+    disabled: PropTypes.bool,
+    outline:PropTypes.bool,
+
+};
+
+ButtonAdd.defaultProps = {
+    label:'Add new',
+    disabled:false,
+    outline:false,
+    size:'md', // sm, md, lg
+};
+
+export const ButtonSave = (props) => {
+    return (
+        <Button
+            onClick={props.onClick}
+            size={props.size}
+            color={'primary'}
+            title={props.title}
+            style={props.style}
+            {...props}
+        >
+            <i className={'fa fa-save'} style={{marginRight:'5px'}}></i> {props.label}
+        </Button>
+    );
+};
+
+ButtonSave.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    label: PropTypes.string,
+    size:PropTypes.string,
+    style: PropTypes.object,
+    title: PropTypes.string,
+    disabled: PropTypes.bool,
+    outline:PropTypes.bool,
+
+};
+
+ButtonSave.defaultProps = {
+    label:'Save',
+    disabled:false,
+    outline:false,
+    size:'md', // sm, md, lg
+};
+
+export const ButtonClose = (props) => {
+    return (
+        <Button
+            onClick={props.onClick}
+            size={props.size}
+            color={'default'}
+            title={props.title}
+            style={props.style}
+            {...props}
+        >
+            <i className={'fa fa-times'} style={{marginRight:'5px'}}></i> {props.label}
+        </Button>
+    );
+};
+
+ButtonClose.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    label: PropTypes.string,
+    size:PropTypes.string,
+    style: PropTypes.object,
+    title: PropTypes.string,
+    disabled: PropTypes.bool,
+    outline:PropTypes.bool,
+
+};
+
+ButtonClose.defaultProps = {
+    label:'Close',
+    disabled:false,
+    outline:false,
+    size:'md', // sm, md, lg
+};
+
+export const ButtonCancel = (props) => {
+    return (
+        <Button
+            onClick={props.onClick}
+            size={props.size}
+            color={'default'}
+            title={props.title}
+            style={props.style}
+            {...props}
+        >
+            <i className={'fa fa-times'} style={{marginRight:'5px'}}></i> {props.label}
+        </Button>
+    );
+};
+
+ButtonCancel.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    label: PropTypes.string,
+    size:PropTypes.string,
+    style: PropTypes.object,
+    title: PropTypes.string,
+    disabled: PropTypes.bool,
+    outline:PropTypes.bool,
+
+};
+
+ButtonCancel.defaultProps = {
+    label:'Cancel',
+    disabled:false,
+    outline:false,
+    size:'md', // sm, md, lg
+};
+
+export const ButtonEdit = (props) => {
+    return (
+        <Button
+            onClick={props.onClick}
+            size={props.size}
+            color={'info'}
+            title={props.title}
+            style={props.style}
+            {...props}
+        >
+            <i className={'fa fa-edit'} style={{marginRight:'5px'}}></i> {props.label}
+        </Button>
+    );
+};
+
+ButtonEdit.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    label: PropTypes.string,
+    size:PropTypes.string,
+    style: PropTypes.object,
+    title: PropTypes.string,
+    disabled: PropTypes.bool,
+    outline:PropTypes.bool,
+
+};
+
+ButtonEdit.defaultProps = {
+    label:'Edit',
+    disabled:false,
+    outline:false,
+    size:'md', // sm, md, lg
+};
+
+export const ButtonDelete = (props) => {
+    return (
+        <Button
+            onClick={props.onClick}
+            size={props.size}
+            color={'danger'}
+            title={props.title}
+            style={props.style}
+            {...props}
+        >
+            <i className={'fa fa-trash'} style={{marginRight:'5px'}}></i> {props.label}
+        </Button>
+    );
+};
+
+ButtonDelete.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    label: PropTypes.string,
+    size:PropTypes.string,
+    style: PropTypes.object,
+    title: PropTypes.string,
+    disabled: PropTypes.bool,
+    outline:PropTypes.bool,
+
+};
+
+ButtonDelete.defaultProps = {
+    label:'Delete',
+    disabled:false,
+    outline:false,
+    size:'md', // sm, md, lg
+};
+
+export const ButtonAction = (props) => {
+    return (
+        <Button
+            onClick={props.onClick}
+            size={props.size}
+            color={props.color}
+            title={props.title}
+            style={props.style}
+            {...props}
+        >
+            {(props.icon)?<i className={'fa fa-'+props.icon} style={{marginRight:'5px'}}></i> : ''}
+            {props.label}
+        </Button>
+    );
+};
+
+ButtonAction.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+    icon:PropTypes.string,
+    size:PropTypes.string,
+    style: PropTypes.object,
+    title: PropTypes.string,
+    disabled: PropTypes.bool,
+    outline:PropTypes.bool,
+
+};
+
+ButtonAction.defaultProps = {
+    label:'Action',
+    icon:'search',
+    disabled:false,
+    outline:false,
+    size:'md', // sm, md, lg
+    color:'warning'
+};
--- src/modules/HomeModule/HomeContainer.js
+++ src/modules/HomeModule/HomeContainer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import NavigationComponent from "../../app_components/complex/navigationComponent/NavigationComponent";
+import {ButtonAction, ButtonAdd, ButtonCancel, ButtonClose, ButtonDelete, ButtonEdit, ButtonSave} from "../../app_components/ButtonsComponents";
 
 export default class HomeContainer extends Component {
     constructor(props) {
@@ -15,6 +16,15 @@ export default class HomeContainer extends Component {
                 <NavigationComponent>
                     <div>
                         home compontent
+                    </div>
+                    <div>
+                        <ButtonAdd onClick={()=>{}}/>
+                        <ButtonClose onClick={()=>{}}/>
+                        <ButtonSave onClick={()=>{}}/>
+                        <ButtonDelete onClick={()=>{}}/>
+                        <ButtonEdit onClick={()=>{}}/>
+                        <ButtonCancel onClick={()=>{}}/>
+                        <ButtonAction onClick={()=>{}} label={'Action'}/>
                     </div>
                 </NavigationComponent>
     );
